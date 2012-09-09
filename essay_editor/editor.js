8b3e2024741045a76b8da1749a0affcc8a28fd24
Frank Jania <fjania@gmail.com>
2012-09-08 23:15:38 -0400

    image support

--- essay_editor/editor.js
+++ essay_editor/editor.js
@@ -14,30 +14,51 @@ function buildSlides(e){
     var slide_content = content.trim().split('\n');
     var slide_len = slide_content.length;
     for (var n=0; n < slide_len; n++){
+        if (slide_content[n].indexOf('[') == 0){
 
-        var line_content = slide_content[n].split('|');
-        var line_len = line_content.length;
-
-        for (var x=0; x < line_len; x++){
-            var text = "";
-            characters_in += line_content[x].length;
-            if (x < line_len){
-                characters_in += 1;
+            sc = slide_content[n];
+            characters_in += sc.length + 1;
+            var imgsrc = "";
+            try {
+                imgsrc = sc.match(/\[(.*)\]/)[1];
+            } catch (err) {
             }
-            for (var y=0; y <= x; y++){
-                text += line_content[y]
-                if (y < x){
-                    text += " ";
-                }
-            }
-            $('#slides').append('<div class="slide">' + text + '</div>');
-            $('#preview').append('<div class="slide" id="preview_slide_' + preview_slide_max +'" style="display: none">' + text + '</div>');
+            console.log(imgsrc)
+            $('#slides').append('<div class="slide"><img src="' + imgsrc + '"/></div>');
+            $('#preview').append('<div class="slide" id="preview_slide_' + preview_slide_max +'" style="display: none"><img src="' + imgsrc + '"/></div>');
             if (caret < characters_in && !previewed){
                 $('#preview_slide_' + preview_slide_max).show();
                 preview_slide_number = preview_slide_max;
                 previewed = true;
             }
             preview_slide_max += 1;
+
+        } else {
+
+            var line_content = slide_content[n].split('|');
+            var line_len = line_content.length;
+
+            for (var x=0; x < line_len; x++){
+                var text = "";
+                characters_in += line_content[x].length;
+                if (x < line_len){
+                    characters_in += 1;
+                }
+                for (var y=0; y <= x; y++){
+                    text += line_content[y]
+                    if (y < x){
+                        text += " ";
+                    }
+                }
+                $('#slides').append('<div class="slide">' + text + '</div>');
+                $('#preview').append('<div class="slide" id="preview_slide_' + preview_slide_max +'" style="display: none">' + text + '</div>');
+                if (caret < characters_in && !previewed){
+                    $('#preview_slide_' + preview_slide_max).show();
+                    preview_slide_number = preview_slide_max;
+                    previewed = true;
+                }
+                preview_slide_max += 1;
+            }
         }
     }
 }
